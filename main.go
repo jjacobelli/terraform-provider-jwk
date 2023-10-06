81f861e57c484a6de94a1449088a8797e0a8b221
Jordan Jacobelli <jordanjacobelli04@gmail.com>
2023-10-06 17:07:11 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"terraform-provider-jwk/jwk"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return jwk.Provider()
+		},
+	})
+}
